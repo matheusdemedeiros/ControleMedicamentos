a6f5917ae56080745fd9c54e420cbb68991536ca
Matheus <Matheus@MATHEUS>
2022-06-14 13:25:32 -0300

    Testes do validador de funcionário implementados.

---
+++ ControleMedicamentos.Dominio.Tests/ModuloFuncionario/ValidadorFuncionarioTest.cs
@@ -0,0 +1,127 @@
+﻿using ControleMedicamentos.Dominio.ModuloFuncionario;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControleMedicamentos.Dominio.Tests.ModuloFuncionario
+{
+    [TestClass]
+    public class ValidadorFuncionarioTest
+    {
+
+        [TestMethod]
+        public void Nome_do_funcionario_deve_ser_obrigatorio()
+        {
+            //arrange
+            var f1 = new Funcionario("", "matheus_user", "m@theuSKl90");
+            var f2 = new Funcionario(null, "matheus_user", "m@theuSKl90");
+            var f3 = new Funcionario("Matheus", "matheus_user", "m@theuSKl90");
+
+            ValidadorFuncionario validador = new ValidadorFuncionario();
+
+            //action
+            var resultado1 = validador.Validate(f1);
+            var resultado2 = validador.Validate(f2);
+            var resultado3 = validador.Validate(f3);
+
+            //assert
+            Assert.AreEqual("O campo 'Nome do funcionário' é obrigatório!", resultado1.Errors[0].ErrorMessage);
+            Assert.AreEqual("O campo 'Nome do funcionário' é obrigatório!", resultado2.Errors[0].ErrorMessage);
+            Assert.AreEqual(0, resultado3.Errors.Count);
+        }
+
+        [TestMethod]
+        public void Nome_do_funcionario_deve_ter_quatro_caracteres_no_minimo()
+        {
+            //arrange
+            var f1 = new Funcionario("zé", "matheus_user", "m@theuSKl90");
+            var f2 = new Funcionario("Matheus", "matheus_user", "m@theuSKl90");
+
+            ValidadorFuncionario validador = new ValidadorFuncionario();
+
+            //action
+            var resultado1 = validador.Validate(f1);
+            var resultado2 = validador.Validate(f2);
+
+            //assert
+            Assert.AreEqual("O campo 'Nome do funcionário' deve ter no mínimo 4 caracteres!", resultado1.Errors[0].ErrorMessage);
+            Assert.AreEqual(0, resultado2.Errors.Count);
+        }
+
+        [TestMethod]
+        public void Login_do_funcionario_deve_ser_obrigatorio()
+        {
+            //arrange
+            var f1 = new Funcionario("Matheus", "", "m@theuSKl90");
+            var f2 = new Funcionario("Matheus", null, "m@theuSKl90");
+            var f3 = new Funcionario("Matheus", "matheus_user", "m@theuSKl90");
+
+            ValidadorFuncionario validador = new ValidadorFuncionario();
+
+            //action
+            var resultado1 = validador.Validate(f1);
+            var resultado2 = validador.Validate(f2);
+            var resultado3 = validador.Validate(f3);
+
+            //assert
+            Assert.AreEqual("O campo 'Login do funcionário' é obrigatório!", resultado1.Errors[0].ErrorMessage);
+            Assert.AreEqual("O campo 'Login do funcionário' é obrigatório!", resultado2.Errors[0].ErrorMessage);
+            Assert.AreEqual(0, resultado3.Errors.Count);
+        }
+
+        [TestMethod]
+        public void Login_do_funcionario_deve_ter_cinco_caracteres_no_minimo()
+        {
+            //arrange
+            Funcionario f1 = new Funcionario("Matheus", "mat", "m@theuSKl90");
+            Funcionario f2 = new Funcionario("Matheus", "matheus_user", "m@theuSKl90");
+
+            ValidadorFuncionario validador = new ValidadorFuncionario();
+
+            //action
+            var resultado1 = validador.Validate(f1);
+            var resultado2 = validador.Validate(f2);
+
+            //assert
+            Assert.AreEqual("O campo 'Login do funcionário' deve ter no mínimo 5 caracteres!", resultado1.Errors[0].ErrorMessage);
+            Assert.AreEqual(0, resultado2.Errors.Count);
+        }
+
+        [TestMethod]
+        public void Senha_do_funcionario_deve_ser_obrigatorio()
+        {
+            //arrange
+            var f1 = new Funcionario("Matheus", "matheus_user", "");
+            var f2 = new Funcionario("Matheus", "matheus_user", null);
+            var f3 = new Funcionario("Matheus", "matheus_user", "m@theuSKl90");
+
+            ValidadorFuncionario validador = new ValidadorFuncionario();
+
+            //action
+            var resultado1 = validador.Validate(f1);
+            var resultado2 = validador.Validate(f2);
+            var resultado3 = validador.Validate(f3);
+
+            //assert
+            Assert.AreEqual("O campo 'Senha do funcionário' é obrigatório!", resultado1.Errors[0].ErrorMessage);
+            Assert.AreEqual("O campo 'Senha do funcionário' é obrigatório!", resultado2.Errors[0].ErrorMessage);
+            Assert.AreEqual(0, resultado3.Errors.Count);
+        }
+
+        [TestMethod]
+        public void Senha_do_funcionario_deve_ter_cinco_caracteres_no_minimo()
+        {
+            //arrange
+            Funcionario f1 = new Funcionario("Matheus", "matheus_user", "m@");
+            Funcionario f2 = new Funcionario("Matheus", "matheus_user", "m@theuSKl90");
+
+            ValidadorFuncionario validador = new ValidadorFuncionario();
+
+            //action
+            var resultado1 = validador.Validate(f1);
+            var resultado2 = validador.Validate(f2);
+
+            //assert
+            Assert.AreEqual("O campo 'Senha do funcionário' deve ter no mínimo 5 caracteres!", resultado1.Errors[0].ErrorMessage);
+            Assert.AreEqual(0, resultado2.Errors.Count);
+        }
+    }
+}
--- ControleMedicamentos.Dominio/ModuloFuncionario/ValidadorFuncionario.cs
+++ ControleMedicamentos.Dominio/ModuloFuncionario/ValidadorFuncionario.cs
@@ -1,8 +1,68 @@
 ﻿using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace ControleMedicamentos.Dominio.ModuloFuncionario
 {
     public class ValidadorFuncionario : AbstractValidator<Funcionario>
     {
+
+        public ValidadorFuncionario()
+        {
+            RuleFor(x => x.Nome)
+                  .NotNull().WithMessage("O campo 'Nome do funcionário' é obrigatório!")
+                  .NotEmpty().WithMessage("O campo 'Nome do funcionário' é obrigatório!");
+
+
+            RuleFor(x => x.Nome)
+                .Custom((nome, context) =>
+                {
+                    if (nome != null)
+                    {
+                        if (nome.Length < 4)
+                            context.AddFailure("O campo 'Nome do funcionário' deve ter no mínimo 4 caracteres!");
+                    }
+                });
+
+            RuleFor(x => x.Nome)
+                .Custom((nome, context) =>
+                {
+                    if (nome != null)
+                    {
+                        if (Regex.IsMatch(nome, "^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$", RegexOptions.IgnoreCase) == false)
+                            context.AddFailure("O campo 'Nome do funcionário' não deve conter números ou caracteres especiais!");
+                    }
+                });
+
+
+            RuleFor(x => x.Login)
+                 .NotNull().WithMessage("O campo 'Login do funcionário' é obrigatório!")
+                 .NotEmpty().WithMessage("O campo 'Login do funcionário' é obrigatório!");
+
+
+            RuleFor(x => x.Login)
+               .Custom((login, context) =>
+               {
+                   if (string.IsNullOrEmpty(login) == false)
+                   {
+                       if (login.Length < 5)
+                           context.AddFailure("O campo 'Login do funcionário' deve ter no mínimo 5 caracteres!");
+                   }
+               });
+
+            RuleFor(x => x.Senha)
+                 .NotNull().WithMessage("O campo 'Senha do funcionário' é obrigatório!")
+                 .NotEmpty().WithMessage("O campo 'Senha do funcionário' é obrigatório!");
+
+
+            RuleFor(x => x.Senha)
+               .Custom((senha, context) =>
+               {
+                   if (string.IsNullOrEmpty(senha) == false)
+                   {
+                       if (senha.Length < 5)
+                           context.AddFailure("O campo 'Senha do funcionário' deve ter no mínimo 5 caracteres!");
+                   }
+               });
+        }
     }
 }
