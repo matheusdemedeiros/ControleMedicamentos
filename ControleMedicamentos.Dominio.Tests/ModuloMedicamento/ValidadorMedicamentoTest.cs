b9504d4f90fb4c18b9115087499d88de4134a4b8
Matheus <Matheus@MATHEUS>
2022-06-14 23:04:21 -0300

    Adicionado o esqueleto das classes de testes unitários de medicamento e requisicao

---
+++ ControleMedicamentos.Dominio.Tests/ModuloMedicamento/ValidadorMedicamentoTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControleMedicamentos.Dominio.Tests.ModuloMedicamento
+{
+
+    [TestClass]
+    public class ValidadorMedicamentoTest
+    {
+        [TestMethod]
+        public void Nome_do_medicamento_deve_ser_obrigatorio()
+        {
+        }
+
+        [TestMethod]
+        public void Nome_do_medicamento_deve_ter_cinco_caracteres_no_minimo()
+        {
+        }
+
+        [TestMethod]
+        public void Descricao_do_medicamento_deve_ser_obrigatorio()
+        {
+        }
+
+        [TestMethod]
+        public void Descricao_do_medicamento_deve_ter_cinco_caracteres_no_minimo()
+        {
+        }
+        
+        [TestMethod]
+        public void Lote_do_medicamento_deve_ser_obrigatorio()
+        {
+        }
+
+        [TestMethod]
+        public void Lote_do_medicamento_deve_ter_cinco_caracteres_no_minimo()
+        {
+        }
+
+        [TestMethod]
+        public void Validade_do_medicamento_deve_ser_obrigatoria()
+        {
+        }
+
+        [TestMethod]
+        public void Validade_do_medicamento_deve_ser_maior_que_a_data_de_criacao()
+        {
+        }
+        
+        [TestMethod]
+        public void Fornecedor_do_medicamento_deve_ser_obrigatorio()
+        {
+        }
+
+    }
+}
---
+++ ControleMedicamentos.Dominio.Tests/ModuloRequisicao/ValidadorRequisicaoTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ControleMedicamentos.Dominio.Tests.ModuloRequisicao
+{
+
+    [TestClass]
+    public class ValidadorRequisicaoTest
+    {
+        [TestMethod]
+        public void Medicaento_da_requisicao_deve_ser_obrigatorio()
+        {
+
+        }
+        [TestMethod]
+        public void Paciente_da_requisicao_deve_ser_obrigatorio()
+        {
+
+        }
+        [TestMethod]
+        public void Funcionario_da_requisicao_deve_ser_obrigatorio()
+        {
+
+        }
+        [TestMethod]
+        public void Data_da_requisicao_deve_ser_obrigatoria()
+        {
+
+        } 
+        
+        [TestMethod]
+        public void Data_da_requisicao_deve_ser_maior_que_a_data_de_criacao_da_requisicao()
+        {
+
+        }
+        
+        [TestMethod]
+        public void QtdMedicamentos_da_requisicao_deve_ser_maior_que_zero()
+        {
+
+        }
+    }
+}
